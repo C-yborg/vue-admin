c4f1ae0a38a709a98f76873cfaefc64666682fc9
solamoda <894742882@qq.com>
2021-02-23 18:16:16 +0800

    登录表单

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,10 +1,36 @@
 <template>
-	<div id="login">123</div>
+	<div id="login">
+		<div class="login-wrap">
+			<ul class="menu-tab">
+				<li v-for="item in menuTab" :key="item.id" :class="{ current: item.current }" @click="toggleMenu(item)">{{item.txt}}</li>
+			</ul>
+		</div>
+		<!-- 表单start -->
+		<!-- 表单end -->
+	</div>
 </template>
 
 <script>
 export default {
 	name: "login",
+	data() {
+		return {
+			menuTab:[
+				{ txt: '登录', current: true },
+				{ txt: '注册', current: false }
+			],
+		}
+	},
+	created() {},
+	mounted() {},
+	methods:{
+		toggleMenu(data) {
+			this.menuTab.forEach(elem => {
+				elem.current = false;
+			})
+			data.current = true;
+		}
+	},
 };
 </script>
 
@@ -12,5 +38,24 @@ export default {
 #login {
 	height: 100vh;
 	background-color: #344a5f;
+}
+.login-wrap {
+	width: 330px;
+	margin: auto;
+}
+.menu-tab {
+	text-align: center;
+	li {
+		display: inline-block;
+		width: 88px;
+		line-height: 36px;
+		font-size: 14px;
+		color: #fff;
+		border-radius: 2px;
+		cursor: pointer;
+	}
+	.current {
+		background-color: rgba(0, 0, 0, .1);
+	}
 }
 </style>
