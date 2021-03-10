e34778258d5bd59c0f82de99482a1182b90f5fad
solamoda <894742882@qq.com>
2021-03-10 18:28:39 +0800

    props

--- src/icons/SvgIcon.vue
+++ src/icons/SvgIcon.vue
@@ -1,20 +1,44 @@
 <template>
-  <svg :class="svgClass" aria-hidden="true">
-    <use :xlink:href="name"></use>
-  </svg>
+    <svg :class="svgClass" aria-hidden="true">
+        <use :xlink:href="iconName"></use>
+    </svg>
 </template>
 
 <script>
 export default {
-  name: "svgIcon",
-  data() {
-      return {
-          svgClass: '',
-          name: ''
-      }
-  }
+    name: "svgIcon",
+    props: {
+      iconClass:{},
+      className:{}
+    },
+    data() {
+        return {
+            name: "",
+        };
+    },
+    methods: {
+        
+    },
+    computed: {
+        iconName() {
+          return `#icon-${this.iconClass}`
+        },
+        svgClass() {
+          if(this.className) {
+            return `svg-icon ${this.className}`
+          } else {
+            return `svg-icon`
+          }
+        }
+    },
 };
 </script>
 
 <style lang='scss' scoped>
+.svg-icon {
+  width: 2em;
+  height: 2em;
+  font-size: 100px;
+  fill: red;
+}
 </style>
--- src/icons/index.js
+++ src/icons/index.js
@@ -8,10 +8,9 @@ Vue.component('svg-icon', SvgIcon)
  * 参数2是否遍历子集目录
  * 参数3定义遍历文件规则
  */
-const req = require.context('./svg', false, /\.svg$/);
+const req = require.context('./svg', false, /\.svg$/); // 获取当前svg目录所有为.svg结尾的文件
 const requireAll = (requireContext) => {
     console.log(requireContext.keys());
-    return requireContext.keys().map(requireContext)
-}
-
-requireAll(req)
+    return requireContext.keys().map(requireContext);  // 解析获取的.svg文件的文件名称，并返回
+};
+requireAll(req);
