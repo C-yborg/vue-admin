0f6a4ae8023943778088c9872b826628405911a0
solamoda <894742882@qq.com>
2021-05-24 18:28:15 +0800

    完善静态样式

--- src/views/Info/dialog/info.vue
+++ src/views/Info/dialog/info.vue
@@ -1,16 +1,32 @@
 <template>
     <el-dialog title="新增" :visible.sync="dialog_info_flag" @close="close" width="580px">
         <el-form :model="form">
-            <el-form-item label="活动名称" :label-width="formLabelWidth">
-                <el-input v-model="form.name" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="活动区域" :label-width="formLabelWidth">
+            <el-form-item label="类别：" :label-width="formLabelWidth">
                 <el-select v-model="form.region" placeholder="请选择活动区域">
                     <el-option label="区域一" value="shanghai"></el-option>
                     <el-option label="区域二" value="beijing"></el-option>
                 </el-select>
             </el-form-item>
+            <el-form-item label="类型：" :label-width="formLabelWidth">
+                <el-input
+                    v-model="form.name"
+                    autocomplete="off"
+                    placeholder="请输入标题"
+                ></el-input>
+            </el-form-item>
+            <el-form-item label="概况：" :label-width="formLabelWidth">
+                <el-input
+                    type="textarea"
+                    v-model="form.name"
+                    autocomplete="off"
+                    placeholder="请输入概况"
+                ></el-input>
+            </el-form-item>
         </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="close">取消</el-button>
+            <el-button type="primary" @click="close">确定</el-button>
+        </div>
     </el-dialog>
 </template>
 
@@ -18,7 +34,7 @@
 export default {
     data() {
         return {
-            dialog_info_flag: true,
+            dialog_info_flag: false,
             form: {
                 name: '',
                 region: '',
